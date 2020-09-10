88c58ed95f46cca3d830ab580e57aeaa68b4939c
Suyog Subedi <subedisuyog555@gmail.com>
2020-09-10 19:12:21 +0545

    Fixed Some typos

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 pomoClock = document.getElementById("pomoClock");
-studyTime = prompt("Study Time? in minutes");
+studyTime = prompt("Enter the time you want to work for. (in minutes)");
 time = studyTime * 60;
-breakTime = prompt("Enter the time you want for break? in minutes");
+breakTime = prompt("Enter the time you want for break. (in minutes)");
 breaks = breakTime * 60;
 
 setInterval(function timer() {
   if (time != 0) {
     time = time - 1;
 
-    pomoClock.innerHTML = time;
+    pomoClock.innerHTML = `Work For ${time} seconds`;
   } else {
     pomoClock.innerHTML = `Break Time`;
     if (breaks != 0) {
       breaks = breaks - 1;
-      pomoClock.innerHTML = `Break Left : ${breaks}`;
+      pomoClock.innerHTML = `Break for : ${breaks}seconds`;
     }
   }
 }, 1000);
